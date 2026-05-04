5d27464c431d38a4b9bd341b702e850f8aee4400
Jonathan Leonhardt <jonathanleonhardt02@gmail.com>
2026-05-04 11:32:58 -0300

    Solucion G5

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/App.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/App.kt
@@ -4,7 +4,7 @@ package edu.dyds.movies.presentation
 
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import edu.dyds.movies.presentation.navigation.Navigation
+import edu.dyds.movies.presentation.Navigation
 
 @Composable
 @Preview
